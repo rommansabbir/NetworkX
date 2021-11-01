cf8fd233543878722a5c905b33044dd6af63a53c
Romman Sabbir <rommansabbir@gmail.com>
2021-11-01 07:29:23 +0600

    Working on Network Bandwidth

--- NetworkX/src/main/java/com/rommansabbir/networkx/NetworkXManager.kt
+++ NetworkX/src/main/java/com/rommansabbir/networkx/NetworkXManager.kt
@@ -7,6 +7,7 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import android.os.Build
 import android.os.Bundle
 
 internal class NetworkXManager constructor(private val application: Application) {
@@ -18,12 +19,32 @@ internal class NetworkXManager constructor(private val application: Application)
 
         override fun onActivityResumed(activity: Activity) {
             try {
-                getConnectivityManager(application).registerNetworkCallback(
-                    getNetworkRequest(),
-                    getNetworkCallBack
-                )
-            }
-            catch (e : Exception){
+                getConnectivityManager(application).apply {
+                    registerNetworkCallback(
+                        getNetworkRequest(),
+                        getNetworkCallBack
+                    )
+                    when (getConnectionType(activity)) {
+                        0 -> {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                            }
+                            else{
+//                                getNetworkCapabilities(activeNetwork)
+                            }
+                        }
+                        1 -> {
+
+                        }
+                        2 -> {
+
+                        }
+                        3 -> {
+
+                        }
+                    }
+                }
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
@@ -31,8 +52,7 @@ internal class NetworkXManager constructor(private val application: Application)
         override fun onActivityPaused(activity: Activity) {
             try {
                 getConnectivityManager(application).unregisterNetworkCallback(getNetworkCallBack)
-            }
-            catch (e : Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
@@ -45,16 +65,55 @@ internal class NetworkXManager constructor(private val application: Application)
 
     }
 
+    fun getConnectionType(context: Context): Int {
+        var result = 0 // Returns connection type. 0: none; 1: mobile data; 2: wifi
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                            result = 2
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                            result = 1
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> {
+                            result = 3
+                        }
+                    }
+                }
+            }
+        } else {
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    when (type) {
+                        ConnectivityManager.TYPE_WIFI -> {
+                            result = 2
+                        }
+                        ConnectivityManager.TYPE_MOBILE -> {
+                            result = 1
+                        }
+                        ConnectivityManager.TYPE_VPN -> {
+                            result = 3
+                        }
+                    }
+                }
+            }
+        }
+        return result
+    }
+
     // Initialize the Manager
     init {
         try {
             getConnectivityManager(application)
             application.registerActivityLifecycleCallbacks(activityCallback)
-        }
-        catch (e : Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
     // Get ConnectivityManager which is a system service
     private fun getConnectivityManager(application: Application) =
         application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -69,6 +128,7 @@ internal class NetworkXManager constructor(private val application: Application)
             .addTransportType(NetworkCapabilities.TRANSPORT_VPN)
             .build()
     }
+
     // Callback to get notified about network availability
     private val getNetworkCallBack = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
