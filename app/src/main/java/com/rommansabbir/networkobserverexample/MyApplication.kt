cae73e3cf86779ff395fb7056093417cb3415b37
Romman Sabbir <rommansabbir@gmail.com>
2022-08-12 18:09:34 +0600

    clean up: removed legacy codes, fix: check internet connectivity status instead of focus on network connection status, lifecycle: network observing lifecycle updated to "Activity CREATE to DESTROY"

--- app/src/main/java/com/rommansabbir/networkobserverexample/MyApplication.kt
+++ app/src/main/java/com/rommansabbir/networkobserverexample/MyApplication.kt
@@ -3,15 +3,10 @@ package com.rommansabbir.networkobserverexample
 import android.app.Application
 import com.rommansabbir.networkx.NetworkXConfig
 import com.rommansabbir.networkx.NetworkXProvider
-import com.rommansabbir.networkx.core.NetworkX
-import com.rommansabbir.networkx.core.NetworkXCore
-import com.rommansabbir.networkx.strategy.NetworkXObservingStrategy
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        /*NetworkXProvider.init(this, true)*/
         /**
          * Initialize NetworkX
          */
@@ -21,26 +16,5 @@ class MyApplication : Application() {
             .withEnableSpeedMeter(true)
             .build()
         NetworkXProvider.enable(builder)
-
-        /**
-         * To Start observing network status using [NetworkX] call [NetworkXCore.init] from application [onCreate] method
-         * Otherwise when try to check if device is connected to the internet or not it will throw an [Exception]
-         * Provide [Application] reference & [NetworkXObservingStrategy] to [NetworkX]
-         *
-         * [NetworkXObservingStrategy] represent delay time in internet connection status checking
-         * Sates are:
-         * [NetworkXObservingStrategy.LOW]
-         * [NetworkXObservingStrategy.HIGH]
-         * [NetworkXObservingStrategy.MEDIUM]
-         * [NetworkXObservingStrategy.REALTIME]
-         */
-//        NetworkXCore.init(this, NetworkXObservingStrategy.MEDIUM)
-
-//        /**
-//         * Also you can provide custom time interval in millis
-//         * Custom State:
-//         * [NetworkXObservingStrategy.CUSTOM]
-//         */
-//        NetworkX.init(this, NetworkXObservingStrategy.CUSTOM(15 * 1000))
     }
 }
