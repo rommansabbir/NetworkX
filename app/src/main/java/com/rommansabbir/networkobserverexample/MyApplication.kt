617d186644ac11f51dd44728c22113492b2ad884
Romman Sabbir <rommansabbir@gmail.com>
2022-08-20 11:31:05 +0600

    application class updated

--- app/src/main/java/com/rommansabbir/networkobserverexample/MyApplication.kt
+++ app/src/main/java/com/rommansabbir/networkobserverexample/MyApplication.kt
@@ -1,20 +1,25 @@
 package com.rommansabbir.networkobserverexample
 
 import android.app.Application
-import com.rommansabbir.networkx.NetworkXConfig
+import com.rommansabbir.networkx.NetworkXLifecycle
 import com.rommansabbir.networkx.NetworkXProvider
+import com.rommansabbir.networkx.SmartConfig
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        /**
-         * Initialize NetworkX
-         */
-        val builder = NetworkXConfig
+        /*Deprecated way*/
+        /*val builder = NetworkXConfig
             .Builder()
             .withApplication(this)
             .withEnableSpeedMeter(true)
             .build()
-        NetworkXProvider.enable(builder)
+        NetworkXProvider.enable(builder)*/
+
+        /*New way*/
+        NetworkXProvider.enable(SmartConfig(this, true, NetworkXLifecycle.Application))
+
+        /*Smart extension*/
+        /*smartEnableNetworkX(SmartConfig(this, true, NetworkXLifecycle.Application))*/
     }
 }
